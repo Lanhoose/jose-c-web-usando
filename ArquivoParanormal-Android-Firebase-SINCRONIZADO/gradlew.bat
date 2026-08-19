e62136edcc8339b5e58fc36c239d3c1721855129
Lanhoose <125027772+Lanhoose@users.noreply.github.com>
2026-08-19 16:26:48 -0300

    Upload ZIP deployment via Web Client
    Uploaded 15 files.

---
+++ ArquivoParanormal-Android-Firebase-SINCRONIZADO/gradlew.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+set "GRADLE_VERSION=8.7"
+set "DIST=%USERPROFILE%\.gradle\wrapper\dists\ge-tech-gradle-%GRADLE_VERSION%\gradle-%GRADLE_VERSION%"
+if not exist "%DIST%\bin\gradle.bat" (
+  echo Gradle bootstrap is intended for Codemagic/Linux. Please run on a machine with Gradle or use Codemagic.
+  exit /b 1
+)
+call "%DIST%\bin\gradle.bat" %*
